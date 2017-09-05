3de7cda508884ee39490b04036dece058397e953
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-09-05 19:16:30 +0200

    With working ChangeDetectorRef

--- mod-6/src/app/person-editor/person-editor.component.ts
+++ mod-6/src/app/person-editor/person-editor.component.ts
@@ -1,30 +1,43 @@
-import { retry } from 'rxjs/operator/retry';
-import { Component, EventEmitter, Input, OnInit, Output, ChangeDetectionStrategy } from '@angular/core';
+import { retry } from "rxjs/operator/retry";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from "@angular/core";
 
 @Component({
-  selector: 'app-person-editor',
-  templateUrl: './person-editor.component.html',
-  styleUrls: ['./person-editor.component.css'],
+  selector: "app-person-editor",
+  templateUrl: "./person-editor.component.html",
+  styleUrls: ["./person-editor.component.css"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PersonEditorComponent implements OnInit {
+  @Input() firstName = "";
+  @Output() firstNameChange = new EventEmitter();
 
-@Input()  firstName = ""
-@Output() firstNameChange  = new EventEmitter();
+  counter = 0;
 
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef) {}
 
   ngOnInit() {
+    setInterval(() => {
+      this.counter++;
+      this.cd.detectChanges();
+    }, 1000);
   }
 
   xxx() {
-    console.count('app-person-editor')
-    return ''
+    console.count("app-person-editor");
+    return "";
   }
 
-  onChange(e){
-    this.firstName = e.target.value
-    console.log(this.firstName)
-    this.firstNameChange.emit(e.target.value)
+  onChange(e) {
+    this.firstName = e.target.value;
+    console.log(this.firstName);
+    this.firstNameChange.emit(e.target.value);
   }
 }
--- mod-6/src/app/person/person.component.ts
+++ mod-6/src/app/person/person.component.ts
@@ -1,31 +1,27 @@
-import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewEncapsulation
+} from "@angular/core";
 
 @Component({
-  selector: 'app-person',
-  templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css'],
+  selector: "app-person",
+  templateUrl: "./person.component.html",
+  styleUrls: ["./person.component.css"],
   encapsulation: ViewEncapsulation.Emulated
 })
 export class PersonComponent implements OnInit {
-
   @Input() person;
-  @Output() onShowMe  = new EventEmitter();
+  @Output() onShowMe = new EventEmitter();
 
-  constructor(private cd : ChangeDetectorRef) { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   showMe() {
-    this.onShowMe.emit('Via emit:' + this.person.firstName)
+    this.onShowMe.emit("Via emit:" + this.person.firstName);
   }
-
-
-  update()
-  {
-    this.cd.detectChanges();
-
-    // this.person.firstName = Date.now().toString();
-  }
-
 }
